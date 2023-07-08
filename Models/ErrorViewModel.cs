98c51bae142ab8b6db948002fac649970d95f8ad
jaafarhawli <jaafarhawle@gmail.com>
2023-07-08 11:05:55 +0300

    initialized MVC ASP.NET core web app and added necessary dependancies

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineBookstore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
